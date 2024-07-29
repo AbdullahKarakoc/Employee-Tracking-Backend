bcac6101363bcd0c81be78a47812ee6fd9f60697
AbdullahKarakoc <abdullahkrkc1453@gmail.com>
2024-07-29 12:28:23 +0300

    feat: add jwtFilter and UserDetailsServiceImpl

---
+++ src/main/java/com/EmployeeTracking/auth/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.EmployeeTracking.auth.security;
+
+import com.EmployeeTracking.auth.user.EmployeeRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final EmployeeRepository repository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
